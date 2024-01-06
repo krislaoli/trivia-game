db0bcd32548e821baf136c47e8196a544ee6ac4f
Pak Coding <laolikristian1305@gmail.com>
2024-01-07 03:29:18 +0700

    Updated

--- pages/StartGame.tsx
+++ pages/StartGame.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import { Image, View, StyleSheet, Text, TouchableOpacity } from "react-native";
+// import React from "react";
+import {
+  Image,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
 import Avatar from "../components/Avatar";
 import { StatusBar } from "expo-status-bar";
 import { FaEdit } from "react-icons/fa";
 import { IoDiamond } from "react-icons/io5";
+import React, { useState } from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
 interface DataAvatar {
   id: string;
@@ -12,6 +21,10 @@ interface DataAvatar {
 }
 
 const StartGame = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   const data: DataAvatar[] = [
     { id: "1", name: "John", imageUrl: "../assets/avatar1.png" },
   ];
@@ -24,16 +37,13 @@ const StartGame = () => {
     <View style={styles.container}>
       <Image style={styles.background} source={require("../assets/bg2.png")} />
       <Image style={styles.logo1} source={require("../assets/logo1.png")} />
-
       <StatusBar style="auto" />
-
-//set diamond
       <View style={styles.diamondContainer}>
         <TouchableOpacity style={styles.diamondButton}>
           <Text style={styles.diamondText}>20</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={toggleModal}>
           <Image
             style={styles.vectordiamond}
             source={require("../assets/adddiamondpng.png")}
@@ -45,20 +55,41 @@ const StartGame = () => {
           source={require("../assets/diamond.png")}
         />
       </View>
-
       <View>
         <Image
           style={styles.avatar}
           source={require("../assets/avatar1.png")}
         />
-        <FaEdit style={styles.edit} />
+        <TouchableOpacity style={styles.edit} onPress={toggleModal}>
+          <FaEdit />
+        </TouchableOpacity>
         <Text style={styles.textup}>Select Your Avatar</Text>
       </View>
-
+      {/* <TouchableOpacity
+              style={styles.closeButton}
+              onPress={toggleModal}
+            >
+              <FontAwesome name="times" size={24} color="black" />
+            </TouchableOpacity> */}
       <TouchableOpacity style={styles.button}>
         <Text style={styles.text}>Play Game</Text>
       </TouchableOpacity>
-
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={toggleModal}
+      >
+        <View style={styles.modalContainer}>
+          {/* Modal content */}
+          <View style={styles.modalContent}>
+            <TouchableOpacity style={styles.modalButton} onPress={toggleModal}>
+              <FontAwesome style={styles.modalButtonText} name="times" size={24} color="black" width={100}/>
+            </TouchableOpacity>
+            <Text style={styles.modalText}>dicoba dulu guys</Text>
+          </View>
+        </View>
+      </Modal>
       <Image style={styles.artist} source={require("../assets/artist.png")} />
     </View>
   );
@@ -168,8 +199,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 60,
     right: -5,
-    width: 30,
-    height: 30,
+    fontSize: 30,
     color: "white",
   },
   artist: {
@@ -178,4 +208,43 @@ const styles = StyleSheet.create({
     height: 190,
     marginTop: "132%",
   },
+
+  // modal edit start
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+    height: 500,
+    width: 400,
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  modalButton: {
+    padding: 1,
+    backgroundColor: "red",
+    borderRadius: 10,
+    alignItems: "center",
+    height: 30,
+    width: 30,
+    marginTop: -10,
+    marginLeft: 340,
+  },
+  modalButtonText: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 26,
+    // marginTop: -20,
+    // marginLeft: 350,
+  },
+
 });
+// modal edit end
+
